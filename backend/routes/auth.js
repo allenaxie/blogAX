86c10c3b62739fc87c8140bb14af2285a5245a38
allenaxie <allenaxie1@gmail.com>
2022-04-03 15:33:39 -0700

    Install more npm packages. Set up validators for signup function.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const {signup} = require('../controllers/auth');
 
+// validators
+const {runValidation} = require('../validators');
+const {userSignupValidator} = require('../validators/auth');
+
 //  POST '/api/signup'
-router.post('/signup', signup);
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router;
