d786f7f215fe88828f7af6666ee1a3d18bc908a6
allenaxie <allenaxie1@gmail.com>
2022-04-04 14:20:23 -0700

    Implement user signup function in MongoDB.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,32 @@
+const User = require('../models/user');
+const shortId = require('shortid');
+
 exports.signup = (req,res) => {
-    const {name, email, password} = req.body;
-    res.json({
-        user: {name, email, password}
-    });
+    // search database for user based on email
+    User.findOne({ email: req.body.email }).exec((err, user) => {
+        // if a user signed up with the same email already
+        if (user) {
+            return res.status(400).json({
+                error: 'Email is already taken. Please use another email address.'
+            });
+        }
+        
+        // if user doesn't exist yet
+        const { name, email, password } = req.body
+        let username = shortId.generate()
+        let profile = `${process.env.CLIENT_URL}/profile/${username}`
+
+        // create new user
+        let newUser = new User({ name, email, password, profile, username })
+        newUser.save((err, success) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            res.json({ 
+                message: 'Signup success!'
+            })
+        })
+    })
 };
