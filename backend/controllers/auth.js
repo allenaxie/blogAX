f69de2459f3804e43e4da27ac8e8f2b1530846df
allenaxie <allenaxie1@gmail.com>
2022-04-04 16:25:00 -0700

    Create sign in function and authentication in backend with JWT.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const shortId = require('shortid');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 exports.signup = (req,res) => {
     // search database for user based on email
@@ -30,3 +32,34 @@ exports.signup = (req,res) => {
         })
     })
 };
+
+exports.signin = (req,res) => {
+    const { email, password } = req.body
+    // check if user exist
+    User.findOne({ email }).exec((err, user) => {
+        // if error or user does not exist in database
+        if(err || !user) {
+            return res.status(400).json({
+                error: "User with that email does not exist. Please signup."
+            })
+        }
+        // authenticate 
+        // if email and password does not match
+        if(!user.authenticate(password)) {
+            return res.status(400).json({
+                error: "Email and password do not match."
+            })
+        }
+        // generate a token and send to client
+        const token = jwt.sign({ _id: user._id}, process.env.JWT_SECRET, {expiresIn:'1d'} );
+
+        res.cookie('token', token, {expiresIn: '1d'})
+        const { _id, username, name, email, role} = user
+        return res.json({
+            token, 
+            // leave out hashed_password from json data
+            user: {_id, username, name, email, role }
+        })
+    });
+
+}
